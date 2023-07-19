612a1b0e9b3beb54385e5be800fce4638d5fd34f
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-07-19 21:43:28 +0530

    db

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const {connect} = require("./db/mongodb");
 require('dotenv').config();
 
 const app = express();
 
 
-app.listen(process.env.port,()=>{
+app.listen(process.env.port,async()=>{
+   await connect
    console.log(`server is running at port ${process.env.port}`);
 })
