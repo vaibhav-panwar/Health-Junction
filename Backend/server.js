a0ec0ac40e311157ca5b39aa0efdab688d092ecd
Shreyansh-Jain7 <shreyansh712jain@gmail.com>
2023-07-19 23:03:54 +0530

    user auth

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const {connect} = require("./db/mongodb");
 require('dotenv').config();
-
+const {userRouter}=require("./routes/user.routes");
+const {auth}=require("./middlewares/auth.middleware");
 const app = express();
+const cors=require("cors");
+app.use(express.json());
 
+app.use(cors());
+app.get("/",(req,res)=>{
+    res.status(200).send("Welcome to Healhub");
+})
+
+app.use("/users",userRouter);
 
 app.listen(process.env.port,async()=>{
    await connect
